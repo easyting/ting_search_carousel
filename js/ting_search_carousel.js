812b1cebc95e6e37502e3fabdc8e99077345c71b
lexor <ao@inlead.dk>
2012-10-12 10:32:14 +0300

    case 905 - JS fix for panels IPE mode.

--- js/ting_search_carousel.js
+++ js/ting_search_carousel.js
@@ -1,8 +1,15 @@
 (function ($) {
   var carousel = false;
-  
+
   Drupal.behaviors.tingSearchCarousel = {
     attach: function(context) {
+      // If panels IPE form ("Cancel" button) fired this attachment we have to ignore it
+      // even in case it is called after form-triggered attachment.
+      if (context.nodeName != '#document' || (window.tsc_noinit != undefined && window.tsc_noinit)) {
+        window.tsc_noinit = true;
+        return;
+      }
+
       carousel_init(0);
 
       $('.search-controller li').click(function() {
@@ -10,7 +17,7 @@
         $(this).addClass('active');
 
         carousel_init($(this).index());
-        
+
         return false;
       });
     }
@@ -23,7 +30,7 @@
       dataType : 'json',
       success : function(msg) {
         $('.ting-search-carousel .subtitle').html(msg.subtitle);
-        
+
         if (!carousel) {
           $('.ting-search-carousel .ting-rs-carousel .rs-carousel-runner').html(msg.content);
           carousel = $('.ting-search-carousel .ting-rs-carousel').carousel();
